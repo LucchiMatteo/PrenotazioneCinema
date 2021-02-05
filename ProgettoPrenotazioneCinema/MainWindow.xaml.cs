d9597519891138be230b60468e0402eb8ab6b10d
LucchiMatteo <mlucchi79@gmail.com>
2021-02-05 11:59:25 +0100

    Aggiunta immagine

---
+++ ProgettoPrenotazioneCinema/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProgettoPrenotazioneCinema
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCassa1_Click(object sender, RoutedEventArgs e)
+        {
+            ell1.Visibility = Visibility.Visible;
+            ell1.Fill = System.Windows.Media.Brushes.Red;
+        }
+    }
+}
